373e82ed911296dfa9ccf1e5036d2619acdc6f29
wawayu1806 <rey1806zz@gmail.com>
2025-02-22 21:31:54 +0100

    Added get methods for input and weight and modified set method

--- hiddenlayer.py
+++ hiddenlayer.py
@@ -11,7 +11,7 @@ class HiddenLayer:
 
     def initialize_weight_matrix(self, next_layer_n):
         mean = 0
-        stddev = np.sqrt(2/(self.n_neurons + next_layer_n))
+        stddev = np.sqrt(2/(self.n_neurons+next_layer_n))
         mat = np.random.normal(loc=mean, scale=stddev, size=(next_layer_n, self.n_neurons))
         self.weight_matrix = mat 
 
@@ -20,10 +20,17 @@ class HiddenLayer:
             self.bias_vector[i,0] = neuron.get_bias() 
 
     def set_input_vector(self, z):
+        z.shape = (self.n_neurons, 1)
         self.input_vector = z
+
+    def get_input_vector(self):
+        return self.input_vector
 
     def get_bias_vector(self):
         return self.bias_vector
+
+    def get_weight_matrix(self):
+        return self.weight_matrix
 
     def update_biases(self, biases):
         for neuron, bias in zip(self.neurons, biases):
--- inputlayer.py
+++ inputlayer.py
@@ -10,12 +10,19 @@ class InputLayer:
 
     def initialize_weight_matrix(self, next_layer_n):
         mean = 0
-        stddev = np.sqrt(2/(self.n_neurons + next_layer_n))
+        stddev = np.sqrt(2/(self.n_neurons+next_layer_n))
         mat = np.random.normal(loc=mean, scale=stddev, size=(next_layer_n, self.n_neurons))
         self.weight_matrix = mat 
 
     def set_input_vector(self, z):
+        z.shape = (self.n_neurons, 1)
         self.input_vector = z
+
+    def get_input_vector(self):
+        return self.input_vector
+
+    def get_weight_matrix(self):
+        return self.weight_matrix
 
     def update_weights(self, weights):
         self.weight_matrix = weights
