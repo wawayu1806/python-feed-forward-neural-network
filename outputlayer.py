53924d39ca7060ce78f46df953fd511a5b1a3bf4
wawayu1806 <rey1806zz@gmail.com>
2025-02-22 21:33:52 +0100

    Added get and set method for output vector and changed class variable

--- outputlayer.py
+++ outputlayer.py
@@ -6,14 +6,18 @@ class OutputLayer:
         self.n_neurons = n_neurons
         self.neurons = [Neuron(0.0) for _ in range(self.n_neurons)] 
         self.bias_vector = np.zeros(shape=(self.n_neurons,1))
-        self.input_vector = None
+        self.output_vector = None
 
     def set_bias_vector(self):
         for i, neuron in enumerate(self.neurons):
             self.bias_vector[i,0] = neuron.get_bias() 
 
-    def set_input_vector(self, z):
-        self.input_vector = z
+    def set_output_vector(self, y):
+        y.shape = (self.n_neurons, 1)
+        self.output_vector = y
+
+    def get_output_vector(self):
+        return self.output_vector
 
     def get_bias_vector(self):
         return self.bias_vector
