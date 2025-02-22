c5d5a6aca040c92bf66dff5a2c0316da0f6dd924
wawayu1806 <rey1806zz@gmail.com>
2025-02-22 21:35:09 +0100

    Added a line testing class method feed_foward

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
     network = Network()
     network.initalize_network([3,4,3])
     network.connect_layers()
+    network.feed_forward(np.array([1,2,3]))
 
 if __name__ == "__main__":
     main()
